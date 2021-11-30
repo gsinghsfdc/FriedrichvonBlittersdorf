8f5fa71980c47bc909ec60aa9d0f1514dd7a957e
gsinghsfdc <gurvindersingh@salesforce.com>
2021-11-30 15:41:35 -0500

    Added front-end stuff

--- app/models/award.rb
+++ app/models/award.rb
@@ -1,12 +1,16 @@
 class Award
-    attr_accessor :name, :image_url
+    attr_accessor :name, :image_url, :type, :color
 
     def self.generate
         types = ['Star', 'Cross', 'Circle', 'Rhombus']
+        colors = ['Gold', 'Silver', 'Bronze', 'Purple']
         page = Wikipedia.find_random
-        page.image_urls.first
+        page.image_urls&.first
         award = new()
         award.name = page.title
         award.image_url = page.image_urls.first
+        award.type = types.sample
+        award.color = colors.sample
+        award
     end
 end
